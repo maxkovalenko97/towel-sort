2c0a2db6aaf828edb41e73b8d9347ec19a65e4b8
Max Kovalenko <tutmrmaxi@gmail.com>
2021-08-29 02:05:17 +0300

    feat: complete tower-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  let result = [];
+  if (matrix == undefined || matrix.length === 0) {
+    return result;
+  }
+  else {
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        let col = i % 2 === 0 ? j : (matrix[i].length - j - 1);
+        result.push(matrix[i][col]);
+      }
+    }
+  }
+  return result;
 }
